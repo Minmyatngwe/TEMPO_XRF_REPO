e9231a89347d4c03f0b713fb2590f3d5e45a7f9d
Minmyatngwe <139677126+Minmyatngwe@users.noreply.github.com>
2026-08-22 23:34:00 +0300

    Fix syntax error in sim.cpp

--- sim.cpp
+++ sim.cpp
@@ -88,7 +88,7 @@ int main(int argc,char **argv){
     }
 
     else {
-        config.debug=false
+        config.debug=false;
         G4String command = "/control/execute ";
         G4String fileName = argv[2];
         uimanager->ApplyCommand(command + fileName);
